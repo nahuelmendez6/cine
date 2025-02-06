d47185b0fd1429fc1620a049211c06edf6703b0d
nahuelmendez6 <mendezmaturano6@gmail.com>
2025-02-05 21:11:00 -0300

    Definicion de clase CustomUser

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
 
 # Create your models here.
+class CustomUser(AbstractUser):
+
+    """
+    Esta clase customiza la clase User por defecto de Django
+    para diferenciar entre un usuario administrador del cine
+    y un usuario final o cliente
+    """
+
+    is_admin = models.BooleanField(default=False)
+    is_customer = models.BooleanField(default=True)
