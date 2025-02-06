f47a4bb7463eecba8acc1a12ac739fbd3fff0447
nahuelmendez6 <mendezmaturano6@gmail.com>
2025-02-06 00:03:32 -0300

    Definicion de modelos Seat, Booking y Ticket

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.db.models import CASCADE
+
+from movies.models import Hall
+from users.models import CustomUser
+from movies.models import Function
 
 # Create your models here.
+class Seat(models.Model):
+
+    """
+    Asientos que el usuario reservera en la compra de su entrada
+    """
+    hall = models.ForeignKey(Hall, on_delete=models.CASCADE)
+    row = models.CharField(max_length=5)
+    number = models.IntegerField()
+    seat_available = models.BooleanField(default=True) # Si el asiento esta disponible se puede seleccionar en la compra
+
+
+class Booking(models.Model):
+
+    user = models.ForeignKey(CustomUser, on_delete=CASCADE)
+    function = models.ForeignKey(Function, on_delete=CASCADE)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2 ,blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=20,
+        choices=[
+            ('pending', 'Pendiente'),
+            ('paid', 'Pagado'),
+            ('cancelled', 'cancelado'),
+            ('expired', 'Expirado')
+        ]
+    )
+
+class Ticket(models.Model):
+
+    """
+    Ticket o entrada del cliente
+    """
+    booking = models.ForeignKey(Booking, on_delete=CASCADE, related_name='tickets')
+    seat = models.ForeignKey(Seat, on_delete=CASCADE)
+    ticket_code = models.CharField(max_length=100, unique=True) # Codigo unico para generar codigo QR
+    issued_at = models.DateTimeField(auto_now_add=True)
+    is_scanned = models.BooleanField(default=False)
+
--- movies/models.py
+++ movies/models.py
@@ -7,15 +7,15 @@ class Movie(models.Model):
     Esta clase representa a cada pelicula
     """
 
-    title = models.CharField(max_length=255)
-    poster = models.ImageField(upload_to="posters/")
-    synopsis = models.TextField()
-    duration = models.IntegerField()
-    genre = models.CharField(max_length=100)
-    classification = models.CharField(max_length=50)
-    trailer_url = models.URLField(blank=True, null=True)
-    date_release = models.DateField()
-    date_finish = models.DateField()
+    title = models.CharField(max_length=255)            # titulo de la pelicula
+    poster = models.ImageField(upload_to="posters/")    # posters/fotos de portada
+    synopsis = models.TextField()                       # breve texto descriptivo
+    duration = models.IntegerField()                    # duracion en minutos
+    genre = models.CharField(max_length=100)            # genero (accion, comedia, animacion...)
+    classification = models.CharField(max_length=50)    # sistema de clasificacion (+18, R)
+    trailer_url = models.URLField(blank=True, null=True)    # Enlace a trailer
+    date_release = models.DateField()                   # a partir de esta fecha la pelicula se mostrara al cliente
+    date_finish = models.DateField()                    # a partir de esta fecha la pelicula se oculta al cliente
     available = models.BooleanField(default=True)
 
 class Hall(models.Model):
@@ -24,9 +24,9 @@ class Hall(models.Model):
     Esta clase representa a cada sala dentro del cine
     """
 
-    name = models.CharField(max_length=50)
-    total_seats = models.IntegerField()
-    available = models.BooleanField(default=True)
+    name = models.CharField(max_length=50)  # nombre de la sala (sala 1, sala 2, ...)
+    total_seats = models.IntegerField()     # total de asientos disponibles
+    available = models.BooleanField(default=True)   # la sala puede estar inhabilitada ciertos dias
 
 class Function(models.Model):
 
@@ -34,9 +34,10 @@ class Function(models.Model):
     Esta clase representa a cada funcion
     """
 
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)      # una pelicula tendra varias funciones
     function_date = models.DateField()
     function_time = models.TimeField()
+    price = models.DecimalField()
     language = models.CharField(max_length=50, choices=[('subtitulada', 'Subtitulada'), ('doblada', 'Doblada')])
     format = models.CharField(max_length=50, choices=[('2D', '2D'), ('3D', '3D'), ('IMAX', 'IMAX')])
     hall = models.ForeignKey(Hall, on_delete=models.CASCADE)
