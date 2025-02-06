d2e19e6a9b604ad3d0173197272a1c8de77e486b
nahuelmendez6 <mendezmaturano6@gmail.com>
2025-02-06 00:14:24 -0300

    Modelo Notification

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+from users.models import CustomUser
+
+
 # Create your models here.
+class Notification(models.Model):
+
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    message = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
