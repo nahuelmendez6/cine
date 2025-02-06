5e608faa4af5a7901e1e62ab540004fffb299f5d
nahuelmendez6 <mendezmaturano6@gmail.com>
2025-02-06 11:16:53 -0300

    Registro de modelos en admin

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Seat, Booking, Ticket, Combo, ComboTicket
 # Register your models here.
+admin.site.register(Seat)
+admin.site.register(Booking)
+admin.site.register(Ticket)
+admin.site.register(Combo)
+admin.site.register(ComboTicket)
