aed2473b7fc8435bc5938fdfd4dae82479c83e98
nahuelmendez6 <mendezmaturano6@gmail.com>
2025-02-08 01:37:50 -0300

    Metdos check_movie_upload y check_function_upload para evitar peliculas y funciones duplicadas

--- movies/services.py
+++ movies/services.py
@@ -6,13 +6,33 @@ No permitir cargar dos veces la misma película en la misma fecha y sala:
 Asegura la correcta gestión de la cartelera.
 """
 
-def check_movie_upload(movie):
-    if Movie.object.filter(title=movie):
+def check_movie_upload(title):
+    if Movie.object.filter(title=title).exists():
         raise ValidationError("Esta película ya ha sido cargada")
 
 
 def check_function_upload(movie, function, hall, function_date, function_time_start, function_time_end,
                           language, format):
-    if Function.object.filter(movie=movie, function=function, hall=hall, function_time_start=function_time_start,
-                              function_date=function_date, function_time_end=function_time_end, language=language, format=format).exists():
+    if Function.object.filter(movie=movie,
+                              function=function,
+                              hall=hall,
+                              function_time_start=function_time_start,
+                              function_date=function_date,
+                              function_time_end=function_time_end,
+                              language=language,
+                              format=format
+                              ).exists():
         raise ValidationError("Esta función ya se ha registrado en el sistema")
+
+    """
+    Verifiacion adicional: evitar solapamiento de funciones en la misma sala
+    """
+
+    overlapping_functions = Function.objects.filter(
+        hall=hall,
+        function_date=function_date,
+        function_time_start=function_time_start,
+        function_time_end=function_time_end
+    )
+    if overlapping_functions.exists():
+        raise ValidationError("El horario de esta función se superpone con otra.")
